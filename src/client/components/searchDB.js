f908b7aee4656214441f801ee4e062cabf94b7fb
kentliu <kent.delatinos@gmail.com>
2022-04-26 12:08:37 +0700

    update ui

--- src/client/components/searchDB.js
+++ src/client/components/searchDB.js
@@ -20,15 +20,15 @@ export default function SearchDB() {
   }, []);
 
   return (
-    <div className="py-0 px-8">
+    <div className="py-0 px-8 bg-[#BBE3ED]">
       <main className="flex flex-col flex-1 min-h-screen py-16 px-0">
         <div className="flex flex-wrap justify-around">
-          <h1 className="text-3xl m-0 text-center">Search Database</h1>
+          <h1 className="text-3xl m-0 text-center text-[#385166] font-semibold">Search Database</h1>
           <div className="flex">
-            <button className="border-2 border-black mx-2 p-1">
+            <button className="mx-2 px-3 bg-[#35C5CE] rounded-3xl text-[#ffffff]">
               <a href="/sickness">Add Sickness</a>
             </button>
-            <button className="border-2 border-black p-1">
+            <button className="px-3 bg-[#35C5CE] rounded-3xl text-[#ffffff]">
               <a href="/test">Test DNA</a>
             </button>
           </div>
@@ -38,7 +38,7 @@ export default function SearchDB() {
             type="text"
             name="query"
             placeholder="Search user"
-            className="p-1 border-2 border-black my-3 w-9/12"
+            className="px-12 my-3 w-9/12 rounded-[42px] drop-shadow-md h-16"
             onChange={(e) => setQuery(e.target.value)}
             value={query}
           />
